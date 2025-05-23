fe9d9048f50ecee3a03875097fd71bcb097c814e
abul-hasan <abulhasan7bd@gmail.com>
2025-05-23 14:57:01 +0600

    dotenvfileadded

--- firebase.init.js
+++ firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvQXEsEmobZcwIMXgoPqRBjXyhwZTo644",
-  authDomain: "modern-app-edab0.firebaseapp.com",
-  projectId: "modern-app-edab0",
-  storageBucket: "modern-app-edab0.firebasestorage.app",
-  messagingSenderId: "1007329929673",
-  appId: "1:1007329929673:web:60adfc38edc6b28a882018",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
